72232caa75043f7175463e899ea9333d86fe149f
Protero <protero@outlook.es>
2021-11-09 15:41:44 +0100

    fix: rename id

---
+++ services/Home/getHouseByCity.js
@@ -0,0 +1,21 @@
+const { getHouseByCity } = require("../../query/home");
+
+module.exports = (db) => async (req, res, next) => {
+	const {id} = req.query;
+	
+    const result = await getHouseByCity(db, { id });
+
+    if(!result){
+        return next({ 
+            statusCode: 400, 
+            error: new Error("Something went wrong.")
+         });
+    }
+	
+	res.status(200).json({
+		success : true,
+        data :{
+            result :result
+        }
+	});
+};
