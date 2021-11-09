c7c83a252cbe61147f02c7844cb21c728032a265
maidersonn <maidersonn@gmail.com>
2021-11-10 00:18:03 +0100

    chore: inserts of city

--- scripts/seeds.js
+++ scripts/seeds.js
@@ -12,6 +12,8 @@ const seed = async () => {
       path.resolve(__dirname, "../sql", "inserts_home_valencia.sql")
     );
     await db.query(sql`${raw(data.toString())}`);
+    await db.query(sql`INSERT INTO city (idCity, city, idCountry, province, weather, safety, avgCost, fun, cooworking, sharemobility, cyclingfriendly, publicTransport, score)
+    VALUES (1, 'Valencia', 1, 'Valencia', 18.0, 4, 2180, 4, True, True, True, True, 5);`);
     console.info("> insertion done! 🚀");
   } catch (error) {
     console.info("> insertion error! ❌");
