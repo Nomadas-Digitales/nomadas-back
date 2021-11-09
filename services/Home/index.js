0988a93d4778efc370fe3af25d4dff6fd71091f0
Protero <protero@outlook.es>
2021-11-09 15:40:43 +0100

    fix: rename route getHouseByCity

--- services/Home/index.js
+++ services/Home/index.js
@@ -1,8 +1,7 @@
 const router = require("express").Router();
 
 module.exports = (db) => {
-  router.get("/list", require("./getHomes")(db));
-  
-
+  router.get("/list", require("./getHouseByCity")(db));
+  router.get("/list/home", require("./getHome")(db));
   return router;
 };
