61cb2272063417613bdcadfa6aefe26c128f2d80
maidersonn <maidersonn@gmail.com>
2021-11-11 22:57:55 +0100

    chore: name of inserts folder

--- scripts/seeds.js
+++ scripts/seeds.js
@@ -9,7 +9,7 @@ const { raw } = require("slonik-sql-tag-raw");
 const seed = async () => {
   try {
     const data = fs.readFileSync(
-      path.resolve(__dirname, "../sql", "inserts_home_valencia_confoto.sql")
+      path.resolve(__dirname, "../sql", "inserts_home.sql")
     );
     await db.query(sql`INSERT INTO city (idCity, city, idCountry, province, weather, safety, avgCost, fun, cooworking, sharemobility, cyclingfriendly, publicTransport, score)
     VALUES (1, 'Valencia', 1, 'Valencia', 18.0, 4, 2180, 4, True, True, True, True, 5);`);
