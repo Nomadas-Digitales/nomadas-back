9eece7040836cebcec3653c7beffa2689afb39de
maidersonn <maidersonn@gmail.com>
2021-11-11 18:36:04 +0100

    chore: services/home folders

--- services/home/index.js
+++ services/home/index.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 
 module.exports = (db) => {
-  router.get("/list", require("./getHouseByCity")(db));
-  router.get("/list/home", require("./getHome")(db));
+  router.get("/list", require("./getAll")(db));
+  router.get("/list/home", require("./getDetail")(db));
+  router.get("/", require("./getByFilters")(db));
   return router;
 };
