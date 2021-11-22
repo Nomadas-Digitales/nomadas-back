03d6bc75b7eb0a1b3e606b9a15b222d33811d88b
maidersonn <maidersonn@gmail.com>
2021-11-22 18:42:02 +0100

    chore: send detail data to front

--- services/home/getDetail.js
+++ services/home/getDetail.js
@@ -1,21 +1,19 @@
 const { getHome } = require("../../query/home");
 
 module.exports = (db) => async (req, res, next) => {
-	const {id} = req.query;
-    	
-    const result = await getHome(db, { id });
+  const { id } = req.query;
 
-    if(!result){
-        return next({ 
-            statusCode: 400, 
-            error: new Error("Something went wrong.")
-         });
-    }
-	
-	res.status(200).json({
-		success : true,
-        data :{
-            result :result
-        }
-	});
+  const result = await getHome(db, { id });
+
+  if (!result) {
+    return next({
+      statusCode: 400,
+      error: new Error("Something went wrong."),
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    data: result,
+  });
 };
